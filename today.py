df18112083dcc807e90b390ef0bd3030b2a2e4d6
Offxc <mcvoidsmp@gmail.com>
2026-05-30 20:35:27 -0400

    Move profile views into left panel

--- today.py
+++ today.py
@@ -400,7 +400,7 @@ def svg_overwrite(filename, age_data, commit_data, star_data, repo_data, contrib
     justify_format(root, 'repo_data', repo_data, 6)
     justify_format(root, 'contrib_data', contrib_data, 4)
     justify_format(root, 'follower_data', follower_data, 10)
-    justify_format(root, 'view_data', view_data, 54)
+    find_and_replace(root, 'view_data', f'{view_data:,}')
     justify_format(root, 'loc_data', loc_data[2], 9)
     justify_format(root, 'loc_add', loc_data[0])
     justify_format(root, 'loc_del', loc_data[1], 7)
